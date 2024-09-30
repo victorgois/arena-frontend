5324d3e5c8b4e4523ae20d2a1c8f33d4c7c56ce3
Victor Góis <victorgois18@gmail.com>
2024-09-30 18:50:15 -0300

    Replace log for error

--- apollo-client.ts
+++ apollo-client.ts
@@ -8,11 +8,11 @@ const httpLink = new HttpLink({
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors)
     graphQLErrors.forEach(({ message, locations, path }) =>
-      console.log(
+      console.error(
         `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
       )
     );
-  if (networkError) console.log(`[Network error]: ${networkError}`);
+  if (networkError) console.error(`[Network error]: ${networkError}`);
 });
 
 const client = new ApolloClient({
