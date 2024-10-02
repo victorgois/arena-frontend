7d7c80c2fb5d13e92ca4d48b76342532af744f3d
Victor Góis <victorgois18@gmail.com>
2024-10-02 17:31:36 -0300

    Remove unnecessary apollo client instance

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
-const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
-  cache: new InMemoryCache(),
-  addTypename: false,
-});
+import client from "./apollo-client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
